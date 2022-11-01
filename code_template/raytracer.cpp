6f9ffb5fb45b688a3913242442e03416e6a0fe5d
emrberk <emreberk5699@gmail.com>
2022-11-01 23:32:06 +0300

    basic color computation implemented

--- code_template/raytracer.cpp
+++ code_template/raytracer.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <climits>
 #include "parser.h"
 #include "ppm.h"
 #include "utils.h"
 
 typedef unsigned char RGB[3];
 parser::Vec3f u;
+parser::Scene scene;
 
 
 
@@ -28,12 +30,107 @@ public:
         this->direction = add(rayPoint,multiply(camera.position,-1));
     }
 
+    float intersect(parser::Face face) {
+        parser::Vec3f a = scene.vertex_data[face.v0_id - 1];
+        parser::Vec3f b = scene.vertex_data[face.v1_id - 1];
+        parser::Vec3f c = scene.vertex_data[face.v2_id - 1];
+
+        float A = determinant(a.x - b.x, a.x - c.x, direction.x, \
+                    a.y - b.y, a.y - c.y, direction.y, \
+                    a.z - b.z, a.z - c.z, direction.z);
+    
+        float betaD = determinant(a.x - origin.x, a.x - c.x, direction.x, \
+                    a.y - origin.y, a.y - c.y, direction.y, \
+                    a.z - origin.z, a.z - c.z, direction.z);
+        float gammaD = determinant(a.x - b.x, a.x - origin.x, direction.x, \
+                    a.y - b.y, a.y - origin.y, direction.y, \
+                    a.z - b.z, a.z - origin.z, direction.z);
+        float tD = determinant(a.x - b.x, a.x - c.x, a.x -origin.x, \
+                    a.y - b.y, a.y - c.y, a.y - origin.y, \
+                    a.z - b.z, a.z - c.z, a.z - origin.z);
+        float beta = betaD / A;
+        float gamma = gammaD / A;
+        float t = tD / A;
+
+        if (beta >= 0 && gamma >= 0 && beta + gamma <= 1) {
+            return t;
+        }
+
+        return -1;
+    }
+
+    float intersect(parser::Mesh mesh) {
+        float minT = INT_MAX;
+        for (auto& face : mesh.faces) {
+            float t = intersect(face);
+            minT = std::min(t, minT);
+        }
+        return minT;
+    }
+
+    float intersect(parser::Sphere sphere) {
+        float A, B, C;
+        float delta;
+        float result;
+        parser::Vec3f center = scene.vertex_data[sphere.center_vertex_id - 1];
+        //printVec(center);
+        float radius = sphere.radius;
+
+        C = pow(origin.x - center.x, 2) + pow(origin.y - center.y, 2) + pow(origin.z - center.z, 2) - pow(radius, 2);
+        B = 2 * direction.x * (origin.x - center.x) + 2 * direction.y * (origin.y - center.y) + 2 * direction.z * (origin.z - center.z);
+        A = pow(direction.x, 2) + pow(direction.y, 2) + pow(direction.z, 2);
+
+        delta = pow(B, 2) - 4 * A * C;
+
+        if (delta < 0){
+            return -1;
+        } else if (delta == 0) {
+            result = -B / (2 * A);
+        } else {
+            delta = sqrt(delta);
+            float t1 = (-B + delta) / (2 * A);
+            float t2 = (-B - delta)  / (2 * A);
+            result = std::min(t1, t2);
+        }
+        return result;
+    }
+
+    parser::Vec3f computeColor() {
+        float minT = INT_MAX;
+        parser::Material* material = nullptr;
+        parser::Vec3f color  = convert(scene.background_color);
+        for (auto& sphere : scene.spheres) {
+            float t = intersect(sphere);
+            if (t >= 0 && t < minT) {
+                minT = t;
+                material = &scene.materials[sphere.material_id - 1];
+            }
+        }
+        for (auto& triangle : scene.triangles) {
+            float t = intersect(triangle.indices);
+            if (t >= 0 && t < minT) {
+                minT = t;
+                material = &scene.materials[triangle.material_id - 1];
+            }
+        }
+        for (auto& mesh : scene.meshes) {
+            for (auto& face : mesh.faces) {
+                float t = intersect(face);
+                if (t >= 0 && t < minT) {
+                    minT = t;
+                    material = &scene.materials[mesh.material_id - 1];
+                }
+            }
+        }
+        // ambient + diffuse + spectacular calculation
+
+        return material ? material->ambient : color;
+    }
 };
 
 int main(int argc, char* argv[])
 {
     // Sample usage for reading an XML scene file
-    parser::Scene scene;
 
     scene.loadFromXml(argv[1]);
     // The code below creates a test pattern and writes
@@ -54,36 +151,35 @@ int main(int argc, char* argv[])
         {   0,   0, 255 },  // Blue
         {   0,   0,   0 },  // Black
     };
-    /*
-    for(auto& camera : scene.cameras) {
-        u = cross(camera.gaze, camera.up);
-    }
-    */
 
-    parser::Camera first = scene.cameras[0];
-    u = cross(first.gaze, first.up);
-    Ray* r1 = new Ray(first, 0, 0);
-    Ray r = *r1;
-    std::cout << r.origin.x << " " << r.origin.y << " " << r.origin.z << std::endl;
-    std::cout << r.direction.x << " " << r.direction.y << " " << r.direction.z << std::endl;
-    int width = 640, height = 480;
-    int columnWidth = width / 8;
-
-    unsigned char* image = new unsigned char [width * height * 3];
-    /*
     int i = 0;
-    for (int y = 0; y < height; ++y)
-    {
-        for (int x = 0; x < width; ++x)
-        {
-            int colIdx = x / columnWidth;
-            image[i++] = BAR_COLOR[colIdx][0];
-            image[i++] = BAR_COLOR[colIdx][1];
-            image[i++] = BAR_COLOR[colIdx][2];
-        }
-    }
-    
+    for (auto& camera : scene.cameras) {
+        u = cross(camera.gaze, camera.up);
+        unsigned char* image = new unsigned char [camera.image_width * camera.image_height * 3];
+        for (int y = 0; y < camera.image_height; ++y) {
+            for (int x = 0; x < camera.image_width; ++x) {
+                /*
+                int colIdx = x / columnWidth;
+                image[i++] = BAR_COLOR[colIdx][0];
+                image[i++] = BAR_COLOR[colIdx][1];
+                image[i++] = BAR_COLOR[colIdx][2];
+                */
 
-    write_ppm("test.ppm", image, width, height);
-   */
+                Ray* r = new Ray(camera, x, y);
+
+                parser::Vec3f color = r->computeColor();
+                
+                image[(y * camera.image_width + x) * 3] = int(color.x * 255 + 0.5);
+                image[(y * camera.image_width + x) * 3 + 1] = int(color.y * 255 + 0.5);;
+                image[(y * camera.image_width + x) * 3 + 2] = int(color.z * 255 + 0.5);;
+                delete r;
+            }
+        }
+        std::string name = "test";
+        name.append(std::to_string(i));
+        name.append(".ppm");
+        const char* c = name.c_str();
+        write_ppm(c, image, camera.image_width, camera.image_height);
+        i++;
+    }
 }
--- code_template/utils.h
+++ code_template/utils.h
@@ -1,24 +1,69 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-#include <math.h>
+#include <cmath>
+#include <iostream>
 
 template<class T>
-T multiply(T first, float scalar);
+T multiply(T first, float scalar) {
+    T result;
+    result.x = first.x * scalar;
+    result.y = first.y * scalar;
+    result.z = first.z * scalar;
+    return result;
+}
 
 template<class T>
-T add(T first, T second);
+T add(T first, T second) {
+    T result;
+    result.x = first.x + second.x;
+    result.y = first.y + second.y;
+    result.z = first.z + second.z;
+    return result;
+}
 
 template<class T>
-float dot(T first, T second);
+float dot(T first, T second) {
+    return first.x * second.x + first.y * second.y + first.z * second.z;
+}
 
 template<class T>
-T cross(T vec1, T vec2);
+T cross(T vec1, T vec2) {
+    T result;
+    result.x = vec1.y * vec2.z - vec1.z * vec2.y;
+    result.y = vec1.x * vec2.z - vec1.z * vec2.x;
+    result.z = vec1.x * vec2.y - vec1.y * vec2.x;
+    return result;
+}
 
 template<class T>
-float length(T vec);
+float length(T vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
 
 template<class T>
-T normalize(T vector);
+T normalize(T vector) {
+    float dot = dot(vector, vector);
+    float k = 1.0 / sqrt(dot);
+    return multiply(vector, k);
+}
+
+parser::Vec3f convert(parser::Vec3i i) {
+    parser::Vec3f result;
+    result.x = i.x;
+    result.y = i.y;
+    result.z = i.z;
+    return result;
+}
+
+template<class T>
+void printVec(T vec) {
+    std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+}
+
+template<class T>
+float determinant(T i00, T i01, T i02, T i10, T i11, T i12, T i20, T i21, T i22) {
+    return i00 * (i11 * i22 - i12* i21) + i10 * (i02 * i21 - i01 * i22) + i20 * (i01 * i12 - i11 * i02);
+}
 
 #endif
